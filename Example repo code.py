29919abf93bdda3316d1db7c23fb793d3c9ce9c3
Masego Mojanaga <masegomojanaga27@gmail.com>
2025-10-30 23:40:14 +0200

    Add files via upload
    Code that is a conditional statement regarding a hot summers' day.

---
+++ Example repo code.py
@@ -0,0 +1,5 @@
+Temparature = str(input("What's the weather like today? Please answer in degrees")) 
+if Temparature <=  str(28):
+    print("Todays is a hot, very hot nice day. Let's go out.")
+else: 
+    print("Let's stay in the house. It's extremely hot. We can get sun burnt")
